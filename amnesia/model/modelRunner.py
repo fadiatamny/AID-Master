d4678843b616d54526b22b92cb6db164c476fefa
or eitan <oreitan@gmail.com>
2021-03-17 21:17:42 +0200

    adding df to text

--- amnesia/model/modelRunner.py
+++ amnesia/model/modelRunner.py
@@ -2,7 +2,9 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 import joblib
 import fasttext
+import numpy as np
 from apiException import ApiException
+
 
 class ModelRunner():
     def __init__(self, fastText: str, knn: str, data: str) -> None:
@@ -19,7 +21,7 @@ class ModelRunner():
             self.fastTextModel = None
             self.knnModel = None
 
-    def changeInstance (self, fastText = '', knn = '') -> None:
+    def changeInstance(self, fastText='', knn='') -> None:
         if fastText != '':
             self.fastTextPath = fastText
         if knn != '':
@@ -27,15 +29,30 @@ class ModelRunner():
 
         self._loadModels()
 
-    def _dfToText(self, df: DataFrame) -> str: 
-        return df.to_json()
+    def _creatdiv(self, raw_frame: DataFrame):
+        categorieslist = list(raw_frame.columns)
+        n =len(categorieslist)
+        k = raw_frame.count(axis='index')
+        s = pd.Series([k],index=[0])
+        s.repeat(n)
+        print(s)
+        return s.reindex(categorieslist,fill_value=k)
+
+    def _dfToText(self, df: DataFrame) -> str:
+        raw_frame = df.replace(0, np.nan)
+        raw_frame = raw_frame.dropna(axis='columns', how='all')
+        del raw_frame['TEXT']
+        res = raw_frame.count()
+        div = self._creatdiv(raw_frame)
+        fin_payload = res.divide(div)
+        return fin_payload.to_json()
 
     def predict(self, text: str):
         if self.fastTextModel is None or self.knnModel is None:
-            self._loadModels()
+          self._loadModels()
         if self.fastTextModel is None or self.knnModel is None:
             raise ApiException(500, 'error occured in server')
-        
+
         fastTextmodel = self.fastTextModel
         knnModel = self.knnModel
         tempDataframe = pd.DataFrame()
@@ -53,3 +70,4 @@ class ModelRunner():
         knnres = knnModel.kneighbors(tempDataframe, return_distance=False)
 
         return self._dfToText(self.categories.loc[knnres[0], :])
+
