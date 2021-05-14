9965188c5896da90ce8c4da9dc347f81d8cc38e6
Fadi Atamny <fadiatamny@gmail.com>
2021-05-14 22:16:35 +0300

    changed folders again

--- fate/src/pages/Test.tsx
+++ fate/src/pages/Test.tsx
@@ -1,4 +1,4 @@
-import Button from '../components/RoundButton/Button'
+import Button from '../components/Button/Button'
 import newGame from '../assets/images/CircleNew.png'
 import uploadGame from '../assets/images/CircleUpload.png'
 
