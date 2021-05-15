b68ae533ec5e7fbfdf9754cc291bb6d055b03a04
Fadi Atamny <fadiatamny@gmail.com>
2021-05-15 19:19:12 +0300

    Revert "Revert "changed architecture to class based and added reconnect ability and added scenario fixes where recieve theme back to the frontend and organized by general category""
    This reverts commit 13a756e5dc11a26649c65bd9f4d2c05cfe44f967.

--- bleach/src/router/api.ts
+++ bleach/src/router/api.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    res.status(200).send('HI')
+    res.status(200).send('AID Master Backend Service')
 })
 
 export default router
