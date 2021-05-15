ece977372118ebd8013f682c2792390806c94d8f
Annieli Siegel <duddoodler@gmail.com>
2021-05-15 19:05:06 +0300

    add inputs, messages, plus minus button and clickable components

--- fate/src/App.tsx
+++ fate/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Test from './pages/Test'
 
 export default function App() {
