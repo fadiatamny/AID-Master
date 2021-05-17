950460b788f582276c842fdf330da5e0187bd0dd
Fadi Atamny <fadiatamny@gmail.com>
2021-05-17 03:33:20 +0300

    created a route to download the file and a controller

--- bleach/src/router/api.ts
+++ bleach/src/router/api.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express'
+import sessionRouter from './session.router'
 
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
     res.status(200).send('AID Master Backend Service')
 })
+
+router.use('/session', sessionRouter)
 
 export default router
