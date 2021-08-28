dee65492e5fe06b50e4f17ae64902bb8f399da22
Fadi Atamny <fadiatamny@gmail.com>
2021-08-28 19:15:58 +0300

    Pre production (#110)
    * set up new envs and added socketio manager support
    
    * added documentation support
    
    * changed to work with .envs
    
    * updated example json to be correct
    
    * Amnesia/fix creator (#109)
    
    * adding spilt data and precentage to the injector
    
    * adding comments
    
    * adding comments
    
    * adding unit test
    
    Co-authored-by: OrEitan <oreitan13@gmail.com>
    
    * changed misstype to mock
    
    * cleanup on fate
    
    * removed unused imports
    
    * removed unused imports and added env examples
    
    Co-authored-by: OrEitan <oreitan13@gmail.com>

--- amnesia/model/dataInjector.py
+++ amnesia/model/dataInjector.py
@@ -18,14 +18,16 @@ if not os.path.isdir(f'{prefix}/logs'):
     os.mkdir(f'{prefix}/logs')
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
-handler = logging.FileHandler(f'{prefix}/logs/logs_injector_{datetime.now().date()}.log')
+handler = logging.FileHandler(
+    f'{prefix}/logs/logs_injector_{datetime.now().date()}.log')
 formatter = '%(asctime)s %(levelname)s -- %(message)s'
 handler.setFormatter(logging.Formatter(formatter))
 logger.addHandler(handler)
 
 
-
 class DataInjector():
+
+    #fetching scenarios from the internet
     @staticmethod
     def fetchCrawledData(numberCrawler):
         urls = [
@@ -36,6 +38,12 @@ class DataInjector():
         crawler = Crawler(urls, numberCrawler)
         return crawler.crawl()
 
+    @staticmethod
+    def _calc_percentage(number: float, percentage: float):
+        percentage_res = (percentage/100)*number
+        return percentage_res
+
+#pre processing the data to get better result and fiting the data structure to fasttext and knn builder
     @staticmethod
     def preProcess(crawledData: list, models, headers):
         predictions = []
@@ -56,11 +64,12 @@ class DataInjector():
 
         return resFrame
 
+#moving the finished fasttext and knn modles to the right location
     @staticmethod
     def _movingData(dataPath: str):
         if len(os.listdir(f'{dataPath}/injectedData/')) != 1:
             raise ModelException(
-                'dataInjector', 'no data in the folder or more thare one file')
+                'dataInjector', 'no data in the folder or more there one file')
         for file in (os.listdir(dataPath)):
             if file.endswith('csv'):
                 os.remove(f'{dataPath}/{file}')
@@ -69,7 +78,8 @@ class DataInjector():
             if file.endswith('csv'):
                 shutil.move(f'{dataPath}/injectedData/{file}',
                             f'{dataPath}/{file}')
-
+                            
+    # clean residual file after the injector finish
     @staticmethod
     def _cleanFiles(dataPath: str, modelsPath: str):
         if len(os.listdir(f'{dataPath}/injectedData/')) > 0:
@@ -80,13 +90,13 @@ class DataInjector():
                 os.remove(file.path)
 
     @staticmethod
-    def inject(dataPath: str, saveModelsPath: str, hash: str,numOfModels:int, modelsPath: str, numberCrawler: int, oldPath: str):    
+    def inject(dataPath: str, saveModelsPath: str, hash: str, numOfModels: int, modelsPath: str, numberCrawler: int, oldPath: str):
         cwd = os.getcwd()
         cwdcat = cwd.partition('model')
         os.chdir(f'{cwdcat[0]}/model/')
         models = ModelUtils.loadFasttextModels(f'{modelsPath}/fasttext')
         currentAccuracy = ModelTester.fastTextTest(
-           dataPath=dataPath,fastTextPath=f'{modelsPath}/fasttext',numberModels=numOfModels)
+            dataPath=dataPath, fastTextPath=f'{modelsPath}/fasttext', numberModels=numOfModels)
         dirPath = os.path.dirname(f'{dataPath}')
         headers = ModelUtils.fetchDatasetHeaders()
         crawledData = DataInjector.fetchCrawledData(numberCrawler)
@@ -101,8 +111,8 @@ class DataInjector():
         ModelBuilder.createKNN(
             f'{dirPath}/injectedData/injectedData_{hash}.csv', f'{saveModelsPath}/knn', 10, f'{hash}')
         accuracy = ModelTester.fastTextTest(
-           dataPath=f'{dirPath}/injectedData/injectedData_{hash}.csv',fastTextPath=f'{saveModelsPath}/fasttext',numberModels=numOfModels)
-        if accuracy > currentAccuracy:
+            dataPath=f'{dirPath}/injectedData/injectedData_{hash}.csv', fastTextPath=f'{saveModelsPath}/fasttext', numberModels=numOfModels)
+        if accuracy > (currentAccuracy-DataInjector._calc_percentage(currentAccuracy, 5)):
             changer = ModelChanger(
                 newPath=saveModelsPath, currentPath=modelsPath, oldPath=oldPath)
             changer.modelsMoving()
@@ -113,10 +123,10 @@ class DataInjector():
         os.chdir(cwd)
 
     @staticmethod
-    def injectionLoop(dataPath: str, saveModelsPath: str, hash: str, modelsPath: str, numberCrawler: int, oldPath: str, loopCount: int = 1,numOfModels:int = 3):
+    def injectionLoop(dataPath: str, saveModelsPath: str, hash: str, modelsPath: str, numberCrawler: int, oldPath: str, loopCount: int = 1, numOfModels: int = 3):
         for i in range(loopCount):
             DataInjector.inject(dataPath=dataPath, saveModelsPath=saveModelsPath, hash=hash,
-                                modelsPath=modelsPath, numberCrawler=numberCrawler, oldPath=oldPath,numOfModels=numOfModels)
+                                modelsPath=modelsPath, numberCrawler=numberCrawler, oldPath=oldPath, numOfModels=numOfModels)
 
 
 if __name__ == '__main__':
@@ -145,7 +155,7 @@ if __name__ == '__main__':
     originalmodels: str = ''
     loop: int = 1
     oldModels: str = ''
-    numOfModels: int = 3 
+    numOfModels: int = 3
 
     for index, item in enumerate(sys.argv, 0):
         if item == '-d' and index + 1 < len(sys.argv):
@@ -174,7 +184,7 @@ if __name__ == '__main__':
 
     try:
         DataInjector.injectionLoop(dataPath=data, saveModelsPath=save, hash=hash,
-                                   modelsPath=originalmodels, numberCrawler=crawlerRounds, oldPath=oldModels,numOfModels=numOfModels)
+                                   modelsPath=originalmodels, numberCrawler=crawlerRounds, oldPath=oldModels, numOfModels=numOfModels)
         # add http call to server to change model based on name and hash.
     except ModelException as e:
         print('Please check -h for help.')
--- bleach/README.md
+++ bleach/README.md
@@ -5,3 +5,5 @@ This is a game service written in Node.TS with usage of Socket.IO.
 The service is responsible for handling messages between participants in game sessions and handling game session dumps.
 
 ### Version 0.14.0
+
+In order to see analytics on the socketio status visit https://admin.socket.io/
